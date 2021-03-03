97bf4c4bfcf32623478050331a9da6f589781c0d
David Snipp <dave@stardraw.com>
2021-03-03 15:52:38 +0000

    latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { VisitComponent } from './visit/visit.component';
+import { AcknowledgementsComponent } from './acknowledgements/acknowledgements.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'pictures', component: PicturesComponent },
+  { path: 'visit', component: VisitComponent },
+  { path: 'acknowledgements', component: AcknowledgementsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { VisitComponent } from './visit/visit.component';
+import { AcknowledgementsComponent } from './acknowledgements/acknowledgements.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { HomeComponent } from './home/home.component';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselModule } from './carousel/carousel.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    VisitComponent,
+    AcknowledgementsComponent,
+    PicturesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbNavModule,
+    CarouselModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
